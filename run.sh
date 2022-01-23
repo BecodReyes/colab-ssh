80b9bf9be277439fa93055856265fab55c7fec10
蓝云Reyes <i@Muir.fun>
2022-01-24 00:40:19 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -44,8 +44,8 @@ getconfig(){
 echo "[Info] 正在获取服务器配置"
 echo "[Info] 配置文件格式：<服务器IP>,<服务器端口>,<认证密码>"
 
-if [ -z "$1" ];do
-	if [ ! -f ".env" ]; then
+if [ -z "$1" ];then
+	if [ ! -f ".env" ];then
 		cp .env /tmp/servers.conf -rf
 		testconfig
 	else
