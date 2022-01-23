7cc74e20e22038f3d6ee2a497ff6aff978cc6cc2
BecodReyes <i@muir.fun>
2022-01-24 00:29:57 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+<div align="center">
+
+# Colab-SSH
+
+</div>
+
+## Status
+- 能用则行
+- 不定期更新
+
+## Feature
+
+
+
+## Usage
+> ```
+> wget -O - https://raw.githubusercontent.com/BecodReyes/colab-ssh/master/run.sh|bash
+> ```
+
+## Disclaimers
+
+本仓库包含的资料仅供参考。
+
+使用本网站时，请务必了解相关条款，若出现相关问题，本人<sup>即BecodReyes</sup>不对该问题负责。
+
+本人<sup>即BecodReyes</sup>不对任何人完全或部分地依据本仓库的全部或部分内容从事的任何事情和因其任何作为或不作为造成的后果承担任何责任。
+
+本人<sup>即BecodReyes</sup>不对任何可以通过此仓库访问的第三方内容承担责任。
---
+++ run.sh
@@ -0,0 +1,110 @@
+#!/bin/bash -e
+
+runfrp(){
+echo "[Info] SSH已启动，请使用下面创建成功的连接信息和密码 "$passwd" 登录到服务器"
+for i in `cat /tmp/servers.conf`;do
+	ip=`echo $i|awk -F',' '{print $1}'`
+	port=`echo $i|awk -F',' '{print $2}'`
+	token=`echo $i|awk -F',' '{print $3}'`
+cat << EOF > /tmp/frpc/$ip
+[common]
+server_addr = $ip
+server_port = $port
+token = $token
+pool_count = 5
+tcp_mux = true
+user = $username
+login_fail_exit = true
+protocol = tcp
+tls_enable = true
+
+[ssh]
+type = tcp
+local_ip = 127.0.0.1
+local_port = 22
+remote_port = $randomport
+use_compression = true
+EOF
+	nohup /tmp/frp/frpc -c /tmp/frpc/$ip > frp_${ip}_log.out &
+	echo "[Info] "$ip":"$port"创建成功"
+	read -p
+done
+}
+
+getconfig(){
+echo "[Info] 正在获取服务器配置"
+wget -qO /tmp/servers.conf $confurl
+if [[ `grep -o ',' /tmp/servers.conf|wc -l` -lt "3" ]];then
+	echo "[Error] 配置文件疑似损坏，请重试"
+else
+	echo "[Info] 配置文件验证成功"
+	runfrp
+fi
+}
+
+getconfig(){
+echo "[Info] 正在获取服务器配置"
+echo "[Info] 请输入服务器配置文件链接"
+echo "[Info] 配置文件格式：<服务器IP>,<服务器端口>,<认证密码>"
+echo "[Info] 只用填充以上部分，剩余我们会帮你处理的"
+read -p "请输入 " confurl
+if [[ $confurl == http* ]];then
+	getconfig
+else
+	"[Error] 配置文件链接不正确!"
+fi
+}
+
+run(){
+echo '    ____                      ______                      '
+echo '   / __ )___  _________  ____/ / __ \___  __  _____  _____'
+echo '  / __  / _ \/ ___/ __ \/ __  / /_/ / _ \/ / / / _ \/ ___/'
+echo ' / /_/ /  __/ /__/ /_/ / /_/ / _, _/  __/ /_/ /  __(__  ) '
+echo '/_____/\___/\___/\____/\__,_/_/ |_|\___/\__, /\___/____/  '
+echo '                                       /____/             '
+echo '    Github: https://github.com/BecodReyes/colab-ssh       '
+echo '                Powered by BecodReyes                     '
+
+echo "[Info] 正在初始化中"
+username=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 6`
+sleep 0.1s
+passwd=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12`
+randomport=`shuf -i 20480-65000 -n 1`
+
+killall sshd
+killall frpc
+
+#获取服务器IP
+server_local=`curl -s http://ip-api.com/line/?lang=zh-CN|sed -n -e 2p -e 5p -e 6p`
+
+echo "[Info] 正在获取openssh"
+apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen -y
+
+echo "[Info] 正在修改ssh设置"
+echo root:$passwd | chpasswd
+mkdir -p /var/run/sshd
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+
+echo "[Info] 正在启动ssh"
+nohup /usr/sbin/sshd -D > sshd_log.out &
+
+echo "[Info] 正在获取frpc"
+wget -qO /tmp/frpc.tar.gz "https://github.com/fatedier/frp/releases/download/v0.38.0/frp_0.38.0_linux_amd64.tar.gz"
+echo "[Info] 正在解压frpc"
+tar --strip-components 1 -zxf /tmp/frpc.tar.gz -C /tmp/
+getconfig
+}
+
+lasttime(){ #watch有点问题，用which+tail
+while true;do
+	cat /proc/uptime | awk '{printf("本次运行剩余时间 : %.2f", 12-$1/60/60)}' > /tmp/lasttime
+	sleep 5s
+done
+}
+
+if [[ "$1" == "last" ]];then
+	lasttime
+else
+	run
+fi
