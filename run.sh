e3488243336067396a6f9f67ad8eec59f07bc111
蓝云Reyes <i@Muir.fun>
2022-02-02 03:10:58 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -100,6 +100,8 @@ echo root:$passwd | chpasswd
 mkdir -p /var/run/sshd
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
+echo "export LD_LIBRARY_PATH" >> /root/.bashrc
 
 echo "[Info] 正在启动ssh"
 nohup /usr/sbin/sshd -D > sshd_log.out 2>&1 &
